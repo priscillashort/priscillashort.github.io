d449179d7d1f1960173ebfb3ee13a9ee54893cac
Unknown <pshort2@cocc.edu>
2017-12-04 19:10:30 -0800

    Version 0.9.9
    Finish python base changer and update

--- Python/BaseChanger/BaseChanger.py
+++ Python/BaseChanger/BaseChanger.py
@@ -1,41 +1,96 @@
 # Base changer function
-# Written by Priscilla Short on 11-29-17
+# Finished by Priscilla Short on 12-04-17
 
-# Base Changer function - This should work for any interger converted to any interger base
-def changeBase(num, base):
-    if type(num) != int or type(base) != int:
-        return 'You have not entered intergers for your arguements. Please only enter intergers.'
+def convertSymToInt(sym):
+    sym = sym.upper()
+    ordVal = ord(sym)
+    if ordVal in range(48, 58):
+        result = ordVal - 48
+    else:
+        result = ordVal - 55
+    return result
+
+def convertIntToSym(num):
+    if num in range(0,10):
+        sym = chr(num + 48)
+    else:
+        sym = chr(num + 55)
+    return sym
+
+def convertFromDecimal(num, base):
+    base = int(base)
     divide = 'place holder'
     result = []
     while divide != 0:
         divide = num // base
         remain = num % base
+        remain = convertIntToSym(remain)
         result.append(str(remain))
         num = divide
     result.reverse()
     result = ''.join(result)
     return result
 
-# A short piece of code to use the function using inputs and error handling
-result1 = 'False'
-result2 = 'False'
-while result1 == 'False' or result2 == 'False':
-    decNum = input('Please enter a decimal interger to convert: ')
-    baseNum = input('Please enter the base you would like your number converted to, using an interger: ')
-    try:
-        int(decNum)
-    except ValueError:
-        print('You have not entered an interger for your number to convert.')
-        result1 = 'False'
-    else:
-        result1 = 'True'
-    try:
-        int(baseNum)
-    except ValueError:
-        print('You have not entered an interger for your base.')
-        result2 = 'False'
-    else:
-        result2 = 'True'
-decNum = int(decNum)
-baseNum = int(baseNum)
-print(decNum, 'in base', baseNum, 'is', changeBase(decNum, baseNum))
+def convertToDecimal(num, base):
+    base = int(base)
+    num = str(num)
+    result = 0
+    for i in range(1, len(num)+1):
+        thisInt = num[i-1]
+        mult = base**(len(num)-i)*convertSymToInt(thisInt)
+        result = result + mult
+    return result
+    
+def convertBaseToNewBase(num, curentBase, newBase):
+    dec = convertToDecimal(num, curentBase)
+    result = convertFromDecimal(dec, newBase)
+    return result
+
+# A short piece of code to test the functions using inputs and error handling
+def testAllFunctions():
+    result1 = False
+    result2 = False
+    result3 = False
+    while result1 == False:
+        oldBase = input('Please enter the base you are converting from, using an integer: ')
+        if oldBase.isdigit() == True:
+            if int(oldBase) > 36 or int(oldBase) < 2:
+                print('You have entered an invalid base. Please limit your base from 2 to 36')
+            else:
+                result1 = True
+        else:
+            print('You have not entered an integer for your base.')
+            
+    while result2 == False:
+        newBase = input('Please enter the base you would like your number converted to, using an integer: ')
+        if newBase.isdigit() == True:
+            if int(newBase) > 36 or int(newBase) < 2:
+                print('You have entered an invalid base. Please limit your base from 2 to 36')
+            else:
+                result2 = True
+        else:
+            print('You have not entered an integer for your base.')
+            
+    while result3 == False:
+        num = input('Please enter a value to convert: ')
+        count0 = 0
+        for i in num:
+            if i.isdigit() == False and i.isalpha() == False:
+                count0 = count0 + 1
+
+        count = 0
+        for i in num:
+            if convertSymToInt(i) >= int(oldBase):
+                count = count + 1
+                
+        if count > 0 or count0 > 0:
+            print('Some of the characters in your number represent values higher than the base you are converting from. This is not a valid number.')
+        
+        if count == 0 and count0 == 0:
+            result3 = True
+
+    print(num, 'converted from base', oldBase, 'to base', newBase, 'is', convertBaseToNewBase(num, oldBase, newBase))
+
+
+testAllFunctions()
+
