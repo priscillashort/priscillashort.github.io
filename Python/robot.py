c974e9799435b2a837408213a1c3a5de4d354275
Unknown <pshort2@cocc.edu>
2017-12-05 19:09:13 -0800

    Version 1.0.2
    Work on robot code

--- Python/robot.py
+++ Python/robot.py
@@ -6,25 +6,70 @@ def move1meter(distance, direction): #distance in intergers of meters - directio
         addleft = 0
         addright = 0
         if direction == 'L':
-            while addleft < distance:
+            while (addleft / 2) < distance:
                 addleft = count + 1
+                if addleft >= distance:
+                        left = left + addleft
+                        break
                 addleft = addleft + addleft
                 left = left + addleft
                 addright = count + 1
                 addright = addright + addright
                 right = right + addright
                 count = count + 1
+                
         if direction == 'R':
-            while addright < distance:
+            while (addright / 2) < distance:
                 addleft = count + 1
                 addleft = addleft + addleft
                 left = left + addleft
                 addright = count + 1
+                if addright >= distance:
+                        right = right + addright
+                        break
                 addright = addright + addright
                 right = right + addright
                 count = count + 1
         return left + right
 
-print(move1meter(50, 'R'))
+def movexand1meter(distance, direction): #distance in intergers of meters - direction L or R
+        left = 0
+        right = 0
+        count = 0
+        addleft = 0
+        addright = 0
+        add = 1
+        if direction == 'L':
+            while (addleft / 2) < distance:
+                addleft = count + add
+                if addleft >= distance:
+                        left = left + addleft
+                        break
+                addleft = addleft + addleft
+                left = left + addleft
+                add = add + 1
+                addright = count + 1
+                addright = addright + addright
+                right = right + addright
+                count = count + 1
+                add = add + 1
+                
+        if direction == 'R':
+            while (addright / 2) < distance:
+                addleft = count + 1
+                addleft = addleft + addleft
+                left = left + addleft
+                add = add + 1
+                addright = count + 1
+                if addright >= distance:
+                        right = right + addright
+                        break
+                addright = addright + addright
+                right = right + addright
+                count = count + 1
+                add = add + 1
+        return left + right
+
+print(movexand1meter(10, 'L'))
 
 
