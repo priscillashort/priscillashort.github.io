dffbd87901816b1e295bfa93d41da063cd40be78
Unknown <pshort2@cocc.edu>
2018-10-30 12:43:25 -0700

    Version 4.2.6
    Add chap 5 excer 3. Do not use slice in recursive binary search.

--- COCCCourses/CS260/binarySearch/CompareBinarySearchesNoSlice.py
+++ COCCCourses/CS260/binarySearch/CompareBinarySearchesNoSlice.py
@@ -4,16 +4,13 @@ import timeit
 
 """
     Priscilla Short
-    Compare recursive binary search to looping binary search using randon ints
+    Compare recursive binary search without slice to looping binary search using randon ints
     
     Description of problem:
-    Use the binary search functions given in the text (recursive and iterative).
-    Generate a random, ordered list of integers and do a benchmark analysis for each one.
-    What are your results? Can you explain them?
-    
-    Answer:
-    the slice operator in Python, used in the recursive version is actually O(k).
-    This means that the binary search using slice will not perform in strict logarithmic time, which makes it slower in its current state.
+    Implement the binary search using recursion without the slice operator.
+    Recall that you will need to pass the list along with the starting and
+    ending index values for the sublist. Generate a random, ordered list of
+    integers and do a benchmark analysis.
 """
 
 def binarySearch(alist, item):
@@ -32,23 +29,22 @@ def binarySearch(alist, item):
                 first = midpoint+1
     return found
 
-def recurBinarySearch(alist, item):
-    if len(alist) == 0:
+def recurBinarySearch(alist, item, start, end):
+    if start == end: #len(alist) == 0:
         return False
     else:
-        midpoint = len(alist)//2
-        if alist[midpoint]==item:
+        midpoint = ((end - start)//2) + start
+        if alist[midpoint] == item:
             return True
         else:
-            if item<alist[midpoint]:
-                return recurBinarySearch(alist[:midpoint],item)
+            if item < alist[midpoint]:
+                return recurBinarySearch(alist, item, start, midpoint)
             else:
-                return recurBinarySearch(alist[midpoint+1:],item)
-
+                return recurBinarySearch(alist, item, midpoint+1, end)
 
 
 print("        Recurse          While Loop")
-recur = timeit.Timer("recurBinarySearch(lst, searchFor)", "from __main__ import recurBinarySearch, lst, searchFor")
+recur = timeit.Timer("recurBinarySearch(lst, searchFor, 0, len(lst))", "from __main__ import recurBinarySearch, lst, searchFor")
 whileL = timeit.Timer("binarySearch(lst, searchFor)", "from __main__ import binarySearch, lst, searchFor")
 for i in range(100000,2000001,100000):
     lst = [item for item in range(i)]
